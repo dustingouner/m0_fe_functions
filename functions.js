51a6785ac724c2a2ce111869bd39012238e1bab9
Dustin Gouner <dustingouner20@gmail.com>
2022-11-10 12:54:40 -0700

    Add functions assignment

--- functions.js
+++ functions.js
@@ -19,17 +19,44 @@ todaysWeather("Sunny")
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 
+function salaryJob(company, num1, num2) {
+    var range = num2 - num1
+    console.log(`For this position at ${company}, the pay range is $ ${range}`)
+  }
+  
+  salaryJob("Google", 100000, 125000)
+
 
 // 4: Write a function that satifies the following interaction pattern:
 
-checkStock(4, "Coffee");
-// => "Coffee is stocked"
+function checkStock(num1, item) {
+    if (num1 === 0) {
+      console.log(`${item} - Out of Stock`)
+    }
+    if (num1 >= 4) {
+      console.log(`${item} is stocked`)
+    }
+    if (num1 <= 3 && num1 >=1) {
+      console.log(`${item} - running LOW`)
+    }
+  }
+  
+  checkStock(4, "Coffee");
+  checkStock(3, "Tortillas");
+  checkStock(0, "Cheese");
+  checkStock(1, "Salsa");
 
-checkStock(3, "Tortillas");
-// => "Tortillas - running LOW"
 
-checkStock(0, "Cheese");
-// => "Cheese - OUT of stock!"
 
-checkStock(1, "Salsa");
-// => "Salsa - running LOW"
+
+// checkStock(4, "Coffee");
+// // => "Coffee is stocked"
+
+// checkStock(3, "Tortillas");
+// // => "Tortillas - running LOW"
+
+// checkStock(0, "Cheese");
+// // => "Cheese - OUT of stock!"
+
+// checkStock(1, "Salsa");
+// // => "Salsa - running LOW"
