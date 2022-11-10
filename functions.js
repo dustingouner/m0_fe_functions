bfeeb7246cac42257e9d07a282945b88f6659632
Dustin Gouner <dustingouner20@gmail.com>
2022-11-10 11:31:35 -0700

    Add functions assignment

--- functions.js
+++ functions.js
@@ -1,8 +1,21 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
 
+function printGreeting() {
+    console.log("Hi, how are you doing?")
+  }
+  
+  printGreeting()
+  printGreeting()
+  printGreeting()
+
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 
+function todaysWeather(type) {
+    console.log(`Today will be ${type}`)
+}
+
+todaysWeather("Sunny")
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 
